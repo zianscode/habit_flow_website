ee3483afa7563771467cb8b025e2f6a1e626e157
Fajar Fauzian <114587943+fajarfauzian@users.noreply.github.com>
2026-05-18 10:44:19 +0700

    next js bug search

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    root: path.resolve(process.cwd()),
+  },
 };
 
 export default nextConfig;
+
